9553e2eaa84c8587491a2bc34cb5afe8629a3e87
dirgepye <s.stenhouse87@gmail.com>
2015-12-01 19:46:55 +0000

    Exercise 1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 
 var db = require('mysql');
 var connection = db.createConnection({
-    user: 'ziad_saab',
+    user: 'dirgepye',
     host: '127.0.0.1',
     database: 'addressbook'
 });
@@ -11,4 +11,31 @@ var connection = db.createConnection({
 var app = express();
 app.use(bodyParser.json());
 
+
+app.get('/hello', function(request, response) {
+
+    connection.query("SELECT * FROM Account", function(err, res) {
+        if (err) {
+            response.send("Error");
+        }
+        else {
+            response.send(res);
+        }
+    });
+    connection.end();
+    //res.send("afewafwe");
+});
+
+
+
 // app.get('...
+
+
+
+
+var server = app.listen(process.env.PORT, process.env.IP, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
